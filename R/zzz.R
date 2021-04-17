98d261401af8d099909da02cdaa106d623ed207d
Thomas Guillerme <guillert@tcd.ie>
2021-04-17 13:10:02 +0000

    version 1.6.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 #Package version checking
 .onAttach <- function(libname = find.package("dispRity"), pkgname = "dispRity") {
-    packageStartupMessage(paste0("       --- dispRity package ---\nThis is the CRAN release version (1.5.0) of the package.\nFor news, vignettes and future releases,\nvisit https://github.com/TGuillerme/dispRity\n"))
+    packageStartupMessage(paste0("       --- dispRity package ---\nThis is the CRAN release version (1.6.0) of the package.\nFor news, vignettes and future releases,\nvisit https://github.com/TGuillerme/dispRity\n"))
 }
