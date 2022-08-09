f1a04368d27a182d86592b3402de098080b4413b
Thomas Guillerme <guillert@tcd.ie>
2022-08-09 09:50:02 +0000

    version 1.7.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 #Package version checking
 .onAttach <- function(libname = find.package("dispRity"), pkgname = "dispRity") {
-    packageStartupMessage(paste0("       --- dispRity package ---\nThis is the CRAN release version (1.6.9) of the package.\nFor news, vignettes and future releases,\nvisit https://github.com/TGuillerme/dispRity\n"))
+    packageStartupMessage(paste0("       --- dispRity package ---\nThis is the CRAN release version (1.7.0) of the package.\nFor news, vignettes and future releases,\nvisit https://github.com/TGuillerme/dispRity\n"))
 }
